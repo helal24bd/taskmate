17aa7dba36de04594052e38b07c24c6a41a6cce3
helal <helal24bd@gmail.com>
2022-09-21 15:03:45 +0600

    updated requerments file

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -133,8 +133,11 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL='todolist'
 LOGIN_URL='login'
 
 django_heroku.setting(locals())
+
